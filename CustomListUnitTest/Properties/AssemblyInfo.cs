f703db23aa96e6958a5da55f8590b406a7b8210e
unknown <mstiff1983@gmail.com>
2020-05-26 11:52:55 -0400

    This is the initial commit where CustomListUnitTest project has been established and is referencing CustomListProj.

---
+++ CustomListUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d90d608-e62e-4869-af33-5af2dc70ecdc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CustomListProj;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    public class CustomListUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
