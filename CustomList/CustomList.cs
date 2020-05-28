66b2b9d9072aa209600e72cca15c192ed25b6a44
unknown <mstiff1983@gmail.com>
2020-05-28 16:26:08 -0400

    I have added an Add method to the CustomList class

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -8,13 +8,42 @@ namespace CustomListProj
 {
     public class CustomList<T>
     {
-        private T[] ourList;
+        private T[] items;
+        public int Count;
+        public int Capacity;
 
         public CustomList()
         {
-            ourList = new T[4];
+            items = new T[4];
+            Count = 0;
+            Capacity = 4;
         }
 
+        public void Add(T item)
+        {
+            //add objects into array
+            //If Count <= Capacity
+            //Increase Capacity if Count is = Capacity
+            //Get items copied to new array
+
+            if (Count == Capacity)
+            {
+                T[] tempArray = new T[Capacity * 2];
+
+                for (int i = 0; i < Capacity; i++)
+                {
+                    tempArray[i] = items[i];
+                   
+                }
+                
+                Capacity *= 2;
+
+                items = tempArray;
+            }
+
+            items[Count] = item;
+            Count++;
+        }
     }
 
        
