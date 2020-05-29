3ad1355e6e45bd8eee0175cf8ba59a27b0ab1eb6
unknown <mstiff1983@gmail.com>
2020-05-29 15:24:19 -0400

    I created an IndexerUnitTest Class and added 5 Test Methods to the class.

---
+++ CustomListUnitTest/IndexerUnitTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using CustomListProj;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    public class IndexerUnitTest
+    {
+        [TestMethod]
+        public void Index_PositiveNumber_ReturnPositiveNumber()
+        {
+            //Arrange
+            CustomList<int> newList = new CustomList<int>();
+            int expected = 3;
+            int actual;
+
+            //Act
+            newList.Add(1);
+            newList.Add(2);
+            newList.Add(3);
+
+            actual = newList[2];
+
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+
+        public void index_string_returnstring()
+        {
+            //Arrange
+            CustomList<string> newList = new CustomList<string>();
+            string expected = "Apple";
+            string actual;
+
+
+            //Act
+            newList.Add("Apple");
+
+            actual = newList[0];
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        [TestMethod]
+
+        public void index_setindexpositivenumber_returnpositivenumber()
+        {
+            //Arrange
+            CustomList<int> newList = new CustomList<int>();
+            int expected = 10;
+            int actual;
+
+            //Act
+            newList[2] = 10;
+            actual = newList[2];
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+
+        public void index_OutofRangeException_ReturnExpectedException()
+        {
+            //Arrange
+            CustomList<int> newList = new CustomList<int>();
+            int actual;
+
+            //Act
+            newList.Add(1);
+            newList.Add(2);
+            newList.Add(3);
+            newList.Add(4);
+
+            //Assert
+            actual = newList[4];
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+
+        public void Index_OutofRangeExceptiononEmptyList_ReturnExpectedException()
+        {
+            //Arrange
+            CustomList<int> newList = new CustomList<int>();
+            int actual;
+
+            //Act
+
+            //Assert
+            actual = newList[0];
+
+        }
+
+
+
+    }
+}
